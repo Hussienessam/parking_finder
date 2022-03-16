63509c1e80b252f832887fb9c062d74114727026
Hussienessam <hussienessam18@gmail.com>
2022-03-16 16:34:28 +0200

    re-adjust gitignore

--- model.py
+++ model.py
@@ -5,11 +5,18 @@ from cvlib.object_detection import draw_bbox
 import os
 
 entries = os.listdir('input/')
+outpath = './output/'
+
+if not os.path.exists(outpath):
+    os.makedirs(outpath)
+
 for entry in entries:
     input_path = "./input/" + entry
     image = cv2.imread(input_path)
+
     box, label, count = cv.detect_common_objects(image)
     output = draw_bbox(image, box, label, count)
     plt.imshow(output)
+
     print("Number of cars in " + entry + " are " + str(label.count('car')))
-    plt.savefig("./output/" + entry)
+    plt.savefig(outpath + entry)
